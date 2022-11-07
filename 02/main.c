b675adf760a4fb492d20d97abd87be61f41db02b
VadymMandrichenko <vadymmandrichenko@gmail.com>
2022-11-07 21:46:40 +0200

    Add task 2, part 2

--- 02/main.c
+++ 02/main.c
@@ -35,80 +35,90 @@ void freeIntArray(IntArray *ptr)
 int main(int argc, char **argv[])
 {
     // optional todo: read and update the input from the file.
-    char *programInString = "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,13,19,1,9,19,23,1,6,23,27,2,27,9,31,2,6,31,35,1,5,35,39,1,10,39,43,1,43,13,47,1,47,9,51,1,51,9,55,1,55,9,59,2,9,59,63,2,9,63,67,1,5,67,71,2,13,71,75,1,6,75,79,1,10,79,83,2,6,83,87,1,87,5,91,1,91,9,95,1,95,10,99,2,9,99,103,1,5,103,107,1,5,107,111,2,111,10,115,1,6,115,119,2,10,119,123,1,6,123,127,1,127,5,131,2,9,131,135,1,5,135,139,1,139,10,143,1,143,2,147,1,147,5,0,99,2,0,14,0\0";
+    char *programInString = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,13,19,1,9,19,23,1,6,23,27,2,27,9,31,2,6,31,35,1,5,35,39,1,10,39,43,1,43,13,47,1,47,9,51,1,51,9,55,1,55,9,59,2,9,59,63,2,9,63,67,1,5,67,71,2,13,71,75,1,6,75,79,1,10,79,83,2,6,83,87,1,87,5,91,1,91,9,95,1,95,10,99,2,9,99,103,1,5,103,107,1,5,107,111,2,111,10,115,1,6,115,119,2,10,119,123,1,6,123,127,1,127,5,131,2,9,131,135,1,5,135,139,1,139,10,143,1,143,2,147,1,147,5,0,99,2,0,14,0\0";
     IntArray tempIntArr, programInIntArr;
 
-    initIntArray(&tempIntArr, 1);
-
-    // fetch
-    while (*programInString != '\0')
+    for (int noun = 0; noun <= 99; noun++)
     {
-        if (*programInString != ',')
+        for (int verb = 0; verb <= 99; verb++)
         {
-            if (*(programInString + 1) == ',' || *(programInString + 1) == '\0')
+            char *pointerToProgramInString = programInString;
+            initIntArray(&tempIntArr, 1);
+
+            // fetch
+            while (*pointerToProgramInString != '\0')
             {
-                insertIntArray(&tempIntArr, *programInString - '0');
-                programInString++;
+                if (*pointerToProgramInString != ',')
+                {
+                    if (*(pointerToProgramInString + 1) == ',' || *(pointerToProgramInString + 1) == '\0')
+                    {
+                        insertIntArray(&tempIntArr, *pointerToProgramInString - '0');
+                        pointerToProgramInString++;
+                    }
+                    else if (*(pointerToProgramInString + 2) == ',' || *(pointerToProgramInString + 2) == '\0')
+                    {
+                        insertIntArray(&tempIntArr, 10 * (*pointerToProgramInString - '0') + *(pointerToProgramInString + 1) - '0');
+                        pointerToProgramInString += 2;
+                    }
+                    else if (*(pointerToProgramInString + 3) == ',' || *(pointerToProgramInString + 3) == '\0')
+                    {
+                        insertIntArray(&tempIntArr, 100 * (*pointerToProgramInString - '0') + 10 * (*(pointerToProgramInString + 1) - '0') + *(pointerToProgramInString + 2) - '0');
+                        pointerToProgramInString += 3;
+                    }
+                }
+                else
+                    pointerToProgramInString++;
             }
-            else if (*(programInString + 2) == ',' || *(programInString + 2) == '\0')
+
+            tempIntArr.array[1] = noun;
+            tempIntArr.array[2] = verb;
+
+            initIntArray(&programInIntArr, tempIntArr.used);
+
+            for (int i = 0; i < programInIntArr.size; i++)
+                insertIntArray(&programInIntArr, tempIntArr.array[i]);
+
+            freeIntArray(&tempIntArr);
+
+            // decode
+            for (int i = 0; i <= programInIntArr.size; i += 4)
             {
-                insertIntArray(&tempIntArr, 10 * (*programInString - '0') + *(programInString + 1) - '0');
-                programInString += 2;
+                if (programInIntArr.array[i] == 99)
+                {
+                    // execute
+                    break;
+                }
+
+                if (programInIntArr.array[i] == 1)
+                {
+                    // execute
+                    int operandAddress1 = programInIntArr.array[i + 1];
+                    int operandAddress2 = programInIntArr.array[i + 2];
+                    int resultAddress = programInIntArr.array[i + 3];
+                    int result = programInIntArr.array[operandAddress1] + programInIntArr.array[operandAddress2];
+                    programInIntArr.array[resultAddress] = result;
+                }
+
+                if (programInIntArr.array[i] == 2)
+                {
+                    // execute
+                    int operandAddress1 = programInIntArr.array[i + 1];
+                    int operandAddress2 = programInIntArr.array[i + 2];
+                    int resultAddress = programInIntArr.array[i + 3];
+                    int result = programInIntArr.array[operandAddress1] * programInIntArr.array[operandAddress2];
+                    programInIntArr.array[resultAddress] = result;
+                }
             }
-            else if (*(programInString + 3) == ',' || *(programInString + 3) == '\0')
-            {
-                insertIntArray(&tempIntArr, 100 * (*programInString - '0') + 10 * (*(programInString + 1) - '0') + *(programInString + 2) - '0');
-                programInString += 3;
+
+            // the answer is 4925 (100 * noun + verb)
+            if (programInIntArr.array[0] == 19690720){
+                printf("noun: %d\r\n", noun);
+                printf("verb: %d", verb);
             }
-        }
-        else
-            programInString++;
-    }
 
-    initIntArray(&programInIntArr, tempIntArr.used);
-
-    for (int i = 0; i < programInIntArr.size; i++)
-        insertIntArray(&programInIntArr, tempIntArr.array[i]);
-
-    freeIntArray(&tempIntArr);
-
-    // decode
-    for (int i = 0; i <= programInIntArr.size; i += 4)
-    {
-        if (programInIntArr.array[i] == 99)
-        {
-            // execute
-            break;
-        }
-
-        if (programInIntArr.array[i] == 1)
-        {
-            // execute
-            int operandAddress1 = programInIntArr.array[i + 1];
-            int operandAddress2 = programInIntArr.array[i + 2];
-            int resultAddress = programInIntArr.array[i + 3];
-            int result = programInIntArr.array[operandAddress1] + programInIntArr.array[operandAddress2];
-            programInIntArr.array[resultAddress] = result;
-        }
-
-        if (programInIntArr.array[i] == 2)
-        {
-            // execute
-            int operandAddress1 = programInIntArr.array[i + 1];
-            int operandAddress2 = programInIntArr.array[i + 2];
-            int resultAddress = programInIntArr.array[i + 3];
-            int result = programInIntArr.array[operandAddress1] * programInIntArr.array[operandAddress2];
-            programInIntArr.array[resultAddress] = result;
+            freeIntArray(&programInIntArr);
         }
     }
-
-    // output
-    for (int i = 0; i < programInIntArr.size; i++)
-    {
-        printf("%d\r\n", programInIntArr.array[i]);
-    }
-
-    freeIntArray(&programInIntArr);
 
     return 0;
 }
