5f36537c98bf714a5dc443d05f1ad2b248ec2814
Vadym-Mandrichenko <vadymmandrichenko@gmail.com>
2023-08-24 22:31:37 +0300

    wip

--- 09/main.c
+++ 09/main.c
@@ -1,183 +1,179 @@
 #include "stdio.h"
 #include "../libs/util.h"
 
-void fetch(char *ptrToProgramString, IntArray *program);
-void decodeAndExecute(Amplifier* amplifier); 
+void fetch(char *p_program_string, IntArray **p_program);
+void decodeAndExecute(IntArray *p_program); 
 
-int main() {
-    printf("Hello, World!");
+int main(int argc, char **argv) {
+    char *p_program_string = readFileOneLine(argv[1]);
+    IntArray *p_program;
+
+    fetch(p_program_string, &p_program);
+    decodeAndExecute(p_program);
+    freeIntArray(p_program);
 
     return 0;
 }
 
-void fetch(char *ptrToProgramString, IntArray *program)
+void fetch(char *p_program_string, IntArray **pp_program)
 {
-    while (*ptrToProgramString != '\0')
+    IntArray *temp_program;
+    initIntArray(temp_program, 1);
+
+    while (*p_program_string != '\0')
     {
-        if (*ptrToProgramString == ',')
+        if (*p_program_string == ',')
         {
-            ptrToProgramString++;
+            p_program_string++;
             continue;
         }
 
         int negativeModifier = 1;
-        if (*ptrToProgramString == '-')
+        if (*p_program_string == '-')
         {
             negativeModifier = -1;
-            ptrToProgramString++;
+            p_program_string++;
         }
 
-        if (*(ptrToProgramString + 1) == ',' || *(ptrToProgramString + 1) == '\0')
+        if (*(p_program_string + 1) == ',' || *(p_program_string + 1) == '\0')
         {
-            insertIntArray(program, negativeModifier * (*ptrToProgramString - '0'));
-            ptrToProgramString++;
+            insertIntArray(temp_program, negativeModifier * (*p_program_string - '0'));
+            p_program_string++;
         }
-        else if (*(ptrToProgramString + 2) == ',' || *(ptrToProgramString + 2) == '\0')
+        else if (*(p_program_string + 2) == ',' || *(p_program_string + 2) == '\0')
         {
-            insertIntArray(program, negativeModifier * (10 * (*ptrToProgramString - '0') + *(ptrToProgramString + 1) - '0'));
-            ptrToProgramString += 2;
+            insertIntArray(temp_program, negativeModifier * (10 * (*p_program_string - '0') + *(p_program_string + 1) - '0'));
+            p_program_string += 2;
         }
-        else if (*(ptrToProgramString + 3) == ',' || *(ptrToProgramString + 3) == '\0')
+        else if (*(p_program_string + 3) == ',' || *(p_program_string + 3) == '\0')
         {
-            insertIntArray(program, negativeModifier * (100 * (*ptrToProgramString - '0') + 10 * (*(ptrToProgramString + 1) - '0') + *(ptrToProgramString + 2) - '0'));
-            ptrToProgramString += 3;
+            insertIntArray(temp_program, negativeModifier * (100 * (*p_program_string - '0') + 10 * (*(p_program_string + 1) - '0') + *(p_program_string + 2) - '0'));
+            p_program_string += 3;
         }
-        else if (*(ptrToProgramString + 4) == ',' || *(ptrToProgramString + 4) == '\0')
+        else if (*(p_program_string + 4) == ',' || *(p_program_string + 4) == '\0')
         {
-            insertIntArray(program, negativeModifier * (1000 * (*ptrToProgramString - '0') + 100 * (*(ptrToProgramString + 1) - '0') + 10 * (*(ptrToProgramString + 2) - '0') + (*(ptrToProgramString + 3) - '0')));
-            ptrToProgramString += 4;
+            insertIntArray(temp_program, negativeModifier * (1000 * (*p_program_string - '0') + 100 * (*(p_program_string + 1) - '0') + 10 * (*(p_program_string + 2) - '0') + (*(p_program_string + 3) - '0')));
+            p_program_string += 4;
         }
-        else if (*(ptrToProgramString + 5) == ',' || *(ptrToProgramString + 5) == '\0')
+        else if (*(p_program_string + 5) == ',' || *(p_program_string + 5) == '\0')
         {
-            insertIntArray(program, negativeModifier * (10000 * (*ptrToProgramString - '0') + 1000 * (*(ptrToProgramString + 1) - '0') + 100 * (*(ptrToProgramString + 2) - '0') + 10 * (*(ptrToProgramString + 3) - '0') + *(ptrToProgramString + 4) - '0'));
-            ptrToProgramString += 5;
+            insertIntArray(temp_program, negativeModifier * (10000 * (*p_program_string - '0') + 1000 * (*(p_program_string + 1) - '0') + 100 * (*(p_program_string + 2) - '0') + 10 * (*(p_program_string + 3) - '0') + *(p_program_string + 4) - '0'));
+            p_program_string += 5;
         }
     }
+
+    initIntArray(*pp_program, temp_program->used);
+    for (int i = 0; i < (*pp_program)->size; i++)
+        insertIntArray(*pp_program, temp_program->array[i]);
+    freeIntArray(temp_program);
 }
 
-void decodeAndExecute(Amplifier* amplifier) 
+void decodeAndExecute(IntArray *p_program) 
 {
-    int inputOpCalls = 0;
-    for (int i = 0; i < amplifier->program->used; i++)
+    int operationAddress = 0;
+    while (operationAddress <= p_program->size)
     {
-        printf("%d,", amplifier->program->array[i]);
-    }
-    printf("\n");
-    while (amplifier->currentOperationAddress <= amplifier->program->used)
-    {
-        int operation = amplifier->program->array[amplifier->currentOperationAddress] % 100, firstMode = amplifier->program->array[amplifier->currentOperationAddress] % 1000 / 100,
-            secondMode = amplifier->program->array[amplifier->currentOperationAddress] % 10000 / 1000, thirdMode = amplifier->program->array[amplifier->currentOperationAddress] % 100000 / 10000;
+        int operation = p_program->array[operationAddress] % 100, firstMode = p_program->array[operationAddress] % 1000 / 100,
+            secondMode = p_program->array[operationAddress] % 10000 / 1000, thirdMode = p_program->array[operationAddress] % 100000 / 10000;
        
         if (operation == 99)
         {
+            // execute
             break;
         }
 
         if (operation == 1)
         {
-            int operand1 = firstMode == 1 ? amplifier->program->array[amplifier->currentOperationAddress + 1] : amplifier->program->array[amplifier->program->array[amplifier->currentOperationAddress + 1]];
-            int operand2 = secondMode == 1 ? amplifier->program->array[amplifier->currentOperationAddress + 2] : amplifier->program->array[amplifier->program->array[amplifier->currentOperationAddress + 2]];
-            int operand3 = amplifier->program->array[amplifier->currentOperationAddress + 3];
+            // execute
+            // todo: extract operand getting to separate function
+            int operand1 = firstMode == 1 ? p_program->array[operationAddress + 1] : p_program->array[p_program->array[operationAddress + 1]];
+            int operand2 = secondMode == 1 ? p_program->array[operationAddress + 2] : p_program->array[p_program->array[operationAddress + 2]];
+            int operand3 = p_program->array[operationAddress + 3];
             int result = operand1 + operand2;
-            amplifier->program->array[operand3] = result;
-            amplifier->currentOperationAddress += 4;
+            p_program->array[operand3] = result;
+            operationAddress += 4;
             continue;
         }
 
         if (operation == 2)
         {
-            int operand1 = firstMode == 1 ? amplifier->program->array[amplifier->currentOperationAddress + 1] : amplifier->program->array[amplifier->program->array[amplifier->currentOperationAddress + 1]];
-            int operand2 = secondMode == 1 ? amplifier->program->array[amplifier->currentOperationAddress + 2] : amplifier->program->array[amplifier->program->array[amplifier->currentOperationAddress + 2]];
-            int operand3 = amplifier->program->array[amplifier->currentOperationAddress + 3];
+            // execute
+            int operand1 = firstMode == 1 ? p_program->array[operationAddress + 1] : p_program->array[p_program->array[operationAddress + 1]];
+            int operand2 = secondMode == 1 ? p_program->array[operationAddress + 2] : p_program->array[p_program->array[operationAddress + 2]];
+            int operand3 = p_program->array[operationAddress + 3];
             int result = operand1 * operand2;
-            amplifier->program->array[operand3] = result;
-            amplifier->currentOperationAddress += 4;
+            p_program->array[operand3] = result;
+            operationAddress += 4;
             continue;
         }
 
         if (operation == 3)
         {
-            if ((amplifier->isFirstCycle == 1 && inputOpCalls > 1) || (amplifier->isFirstCycle == 0 && inputOpCalls > 0))
-                break;
-            int operand1 = amplifier->program->array[amplifier->currentOperationAddress + 1];
-            int result = amplifier->input[inputOpCalls];
-            amplifier->program->array[operand1] = result;
-            amplifier->currentOperationAddress += 2;
-            inputOpCalls++;
+            // execute
+            int operand1 = p_program->array[operationAddress + 1];
+            int result;
+            scanf("%d", &result);
+            p_program->array[operand1] = result;
+            operationAddress += 2;
             continue;
         }
 
         if (operation == 4)
         {
-            int result = firstMode == 1 ? amplifier->program->array[amplifier->currentOperationAddress + 1] : amplifier->program->array[amplifier->program->array[amplifier->currentOperationAddress + 1]];
+            // execute
+            int result = firstMode == 1 ? p_program->array[operationAddress + 1] : p_program->array[p_program->array[operationAddress + 1]];
             printf("%d\r\n", result);
-            amplifier->currentOperationAddress += 2;
-            amplifier->output = result;
+            operationAddress += 2;
             continue;
         }
 
         if (operation == 5)
         {
-            int operand1 = firstMode == 1 ? amplifier->program->array[amplifier->currentOperationAddress + 1] : amplifier->program->array[amplifier->program->array[amplifier->currentOperationAddress + 1]];
-            int operand2 = secondMode == 1 ? amplifier->program->array[amplifier->currentOperationAddress + 2] : amplifier->program->array[amplifier->program->array[amplifier->currentOperationAddress + 2]];
+            // execute
+            int operand1 = firstMode == 1 ? p_program->array[operationAddress + 1] : p_program->array[p_program->array[operationAddress + 1]];
+            int operand2 = secondMode == 1 ? p_program->array[operationAddress + 2] : p_program->array[p_program->array[operationAddress + 2]];
             if (operand1 != 0)
-                amplifier->currentOperationAddress = operand2;
+                operationAddress = operand2;
             else
-                amplifier->currentOperationAddress += 3;
+                operationAddress += 3;
             continue;
         }
 
         if (operation == 6)
         {
-            int operand1 = firstMode == 1 ? amplifier->program->array[amplifier->currentOperationAddress + 1] : amplifier->program->array[amplifier->program->array[amplifier->currentOperationAddress + 1]];
-            int operand2 = secondMode == 1 ? amplifier->program->array[amplifier->currentOperationAddress + 2] : amplifier->program->array[amplifier->program->array[amplifier->currentOperationAddress + 2]];
+            int operand1 = firstMode == 1 ? p_program->array[operationAddress + 1] : p_program->array[p_program->array[operationAddress + 1]];
+            int operand2 = secondMode == 1 ? p_program->array[operationAddress + 2] : p_program->array[p_program->array[operationAddress + 2]];
             if (operand1 == 0)
-                amplifier->currentOperationAddress = operand2;
+                operationAddress = operand2;
             else 
-                amplifier->currentOperationAddress += 3;
+                operationAddress += 3;
             continue;
         }
 
         if (operation == 7)
         {
-            int operand1 = firstMode == 1 ? amplifier->program->array[amplifier->currentOperationAddress + 1] : amplifier->program->array[amplifier->program->array[amplifier->currentOperationAddress + 1]];
-            int operand2 = secondMode == 1 ? amplifier->program->array[amplifier->currentOperationAddress + 2] : amplifier->program->array[amplifier->program->array[amplifier->currentOperationAddress + 2]];
-            int operand3 = amplifier->program->array[amplifier->currentOperationAddress + 3];
+            int operand1 = firstMode == 1 ? p_program->array[operationAddress + 1] : p_program->array[p_program->array[operationAddress + 1]];
+            int operand2 = secondMode == 1 ? p_program->array[operationAddress + 2] : p_program->array[p_program->array[operationAddress + 2]];
+            int operand3 = p_program->array[operationAddress + 3];
             if (operand1 < operand2)
-                amplifier->program->array[operand3] = 1;
+                p_program->array[operand3] = 1;
             else
-                amplifier->program->array[operand3] = 0;
-            amplifier->currentOperationAddress += 4;
+                p_program->array[operand3] = 0;
+            operationAddress += 4;
             continue;
         }
 
         if (operation == 8)
         {
-            int operand1 = firstMode == 1 ? amplifier->program->array[amplifier->currentOperationAddress + 1] : amplifier->program->array[amplifier->program->array[amplifier->currentOperationAddress + 1]];
-            int operand2 = secondMode == 1 ? amplifier->program->array[amplifier->currentOperationAddress + 2] : amplifier->program->array[amplifier->program->array[amplifier->currentOperationAddress + 2]];
-            int operand3 = amplifier->program->array[amplifier->currentOperationAddress + 3];
+            int operand1 = firstMode == 1 ? p_program->array[operationAddress + 1] : p_program->array[p_program->array[operationAddress + 1]];
+            int operand2 = secondMode == 1 ? p_program->array[operationAddress + 2] : p_program->array[p_program->array[operationAddress + 2]];
+            int operand3 = p_program->array[operationAddress + 3];
             if (operand1 == operand2)
-                amplifier->program->array[operand3] = 1;
+                p_program->array[operand3] = 1;
             else
-                amplifier->program->array[operand3] = 0;
-            amplifier->currentOperationAddress += 4;
+                p_program->array[operand3] = 0;
+            operationAddress += 4;
             continue;
         }
-        amplifier->currentOperationAddress = amplifier->currentOperationAddress;
     }
-}
-
-IntArray** generateSettingSequences()
-{
-    IntArray** store = (IntArray**) malloc(sizeof(IntArray*) * 120);
-    IntArray* ptrSequence = (IntArray*) malloc(sizeof(IntArray));
-    initIntArray(ptrSequence, 5);
-    for (int i = 0; i < 5; ++i) 
-    {
-        insertIntArray(ptrSequence, i + 5);
-    }
-    int index = 0;
-    permute(&store, &index, ptrSequence, 0, ptrSequence->used - 1);
-    freeIntArray(ptrSequence);
-    return store;
 }
